3145f80c4a444bc26fe4e1fcb9b59c91a5aeb724
Jeremy Fabre <jeremy.fabre@hotmail.com>
2010-10-26 22:53:59 -0400

    added styles to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
-  
+  default_scope :order => 'created_at DESC'
+
   def replies
-    Comment.all(:conditions => {:reply_to => id})
+    Comment.all(:conditions => {:reply_to => id}, :order => 'created_at ASC')
   end
   def is_root?
     !reply_to || reply_to == 0
