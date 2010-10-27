f063607c842fc6ccb8850bbca9e93d0af634202b
Jeremy Fabre <jeremy.fabre@hotmail.com>
2010-10-26 23:19:17 -0400

    added comment logic

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,4 +8,13 @@ class Comment < ActiveRecord::Base
   def is_root?
     !reply_to || reply_to == 0
   end
+  def text
+    val = read_attribute(:text)
+    if(val)
+      val = val.gsub('\r\n', '<br />')
+      val = val.gsub('http://', '')
+      val = val.gsub(/[^ ][a-zA-Z0-9\-\.]+\.(com|org|net|mil|edu|ca|COM|ORG|NET|MIL|EDU|CA)/, '<a target="blank" href="http://\0">\0</a>')
+    end  
+    val
+  end
 end
