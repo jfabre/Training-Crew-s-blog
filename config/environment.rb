070218c502785d7685dec8a80822bdefd27f417a
Jeremy Fabre <jeremy.fabre@hotmail.com>
2010-12-08 12:26:46 -0500

    Added encoding

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Be sure to restart your server when you modify this file
-#Encoding.default_external = Encoding::UTF_8
+
+Encoding.default_external = Encoding::UTF_8
+
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
