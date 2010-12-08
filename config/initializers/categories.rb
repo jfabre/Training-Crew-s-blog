09207be4ba7278b3164d79bd461d4d341dd772c2
Jeremy Fabre <jeremy.fabre@hotmail.com>
2010-12-08 12:59:38 -0500

    added begin rescue to category initializer

--- config/initializers/categories.rb
+++ config/initializers/categories.rb
@@ -1,8 +1,11 @@
 # encoding: utf-8
 to_create = ['Entrainements', 'Activités']
-if Category.all().size != to_create.size 
-  Category.destroy_all
-  to_create.each do |title|
-    Category.create!(:title => title, :slug => Post.create_slug(title))
+begin
+  if Category.all().size != to_create.size 
+    Category.destroy_all
+    to_create.each do |title|
+      Category.create!(:title => title, :slug => Post.create_slug(title))
+    end
   end
+rescue
 end
