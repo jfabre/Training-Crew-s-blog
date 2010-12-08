b5100b20009bc568879c90ee9f88dac7061a736b
Jeremy Fabre <jeremy.fabre@hotmail.com>
2010-12-08 13:16:27 -0500

    corrected s3 problem

--- config/initializers/s3_config.rb
+++ config/initializers/s3_config.rb
@@ -1,3 +1,9 @@
 # config/initializers/load_config.rb
-S3_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/s3.yml")
 
+if File.exists?("#{RAILS_ROOT}/config/s3.yml")
+  S3_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/s3.yml")
+else
+  S3_CONFIG = {}
+  S3_CONFIG['access_key_id'] = ENV['S3_KEY']
+  S3_CONFIG['secret_access_key'] = ENV['S3_SECRET']
+end
