275e408d2271d563d20627d45fd75243277584db
Jeremy Fabre <jeremy.fabre@hotmail.com>
2010-12-08 08:08:28 -0500

    Encoding might not work in ruby 1.8

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file
-Encoding.default_external = Encoding::UTF_8
+Encoding.default_external = Encoding::UTF_8 unless Encoding.nil?
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
@@ -17,7 +17,7 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  config.gem 'sqlite3-ruby', :lib => 'sqlite3'
+  #config.gem 'sqlite3-ruby', :lib => 'sqlite3'
   config.gem 'activerecord'
   config.gem 'rubyjedi-actionwebservice', :lib => 'actionwebservice'
   config.gem 'will_paginate'
