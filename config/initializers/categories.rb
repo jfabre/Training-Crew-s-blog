85dcafbff90557b6d3f772ec23e3139c27ca6c60
Jeremy Fabre <jeremy.fabre@hotmail.com>
2010-12-09 15:12:39 -0500

    added user creation

--- config/initializers/categories.rb
+++ config/initializers/categories.rb
@@ -1,10 +1,19 @@
 # encoding: utf-8
 to_create = ['Entrainements', 'Activités']
 begin
-  if Category.all().size != to_create.size 
+  if Category.all.size != to_create.size 
     Category.destroy_all
     to_create.each do |title|
       Category.create!(:title => title, :slug => Post.create_slug(title))
+    end
+  end
+rescue
+end
+
+begin 
+  if User.all.size == 0 
+    unless ENV['admin_user'].nil? or ENV['admin_pass'].nil? 
+      User.create({:username => ENV['admin_user'] , :password => ENV['admin_pass']}) 
     end
   end
 rescue
