8e90f78d4c2542d7ea2348b608f02e321d189ef0
Jeremy Fabre <jeremy.fabre@hotmail.com>
2011-01-01 19:36:12 -0500

    added timespan display instead of dates for comments, moved post date on titles

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,12 +12,25 @@ class Comment < ActiveRecord::Base
     val = read_attribute(:text)
     if(val)
       val = val.gsub(/(\r\n|\n)/, '<br />')
-      logger.info val
       val = val.gsub(/(<br \/>){3,20}/, '<br />')
-      logger.info val
       val = val.gsub(/http:\/\//, '')
       val = val.gsub(/[^ ][a-zA-Z0-9\-\.]+\.(com|org|net|mil|edu|ca|COM|ORG|NET|MIL|EDU|CA)/, '<a target="blank" href="http://\0">\0</a>')
     end  
     val
+  end
+  def since
+    created_at = read_attribute(:created_at).to_datetime
+    hours,minutes,seconds = Date.day_fraction_to_time(DateTime.now - created_at)
+    
+    if hours > 7 * 24
+      return created_at.strftime("%Y-%m-%d")
+    elsif (24..(24 * 7)).include?(hours)
+      return "Il y a #{hours / 24} jours"
+    elsif (1..23).include?(hours)
+      return "Il y a #{hours} heures"
+    elsif (1..59).include?(minutes)
+      return "Il y a #{minutes} minutes"
+    end 
+    return "Il y a #{seconds} secondes"
   end
 end
