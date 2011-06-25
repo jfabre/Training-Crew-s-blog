4d633082b4be6b3d5e9a3e11b0778778b02a80eb
Jeremy Fabre <jeremy.fabre@hotmail.com>
2011-06-25 17:37:16 -0400

    refactored since logic on comment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,34 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def format_comment comment
+    if comment
+      comment = format_carriage_return comment
+      comment = format_links comment
+    end  
+    comment
+  end
+  def format_carriage_return comment
+      comment = comment.gsub(/(\r\n|\n)/, '<br />')
+      comment.gsub(/(<br \/>){3,20}/, '<br />')
+  end
+  def format_links comment
+    regex = /[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}[A-Za-z0-9\-\.\+\_\/\%\?\#\&\=]*/
+    comment = comment.gsub(/http:\/\//, '')
+    comment.gsub(regex, '<a target="blank" href="http://\0">\0</a>')
+  end
+  
+  def friendly_display hours, minutes, seconds
+    if (24..(24 * 7)).include?(hours)
+      if (24..47).include?(hours)
+        return "Depuis une journée"
+      else  
+        return "Depuis #{hours / 24} jours"
+      end
+    elsif (1..23).include?(hours)
+      return "Depuis #{pluralize(hours, 'heure')}"
+    elsif (1..59).include?(minutes)
+      return "Depuis #{pluralize(minutes, 'minute')}"
+    end 
+    return "Depuis #{pluralize(seconds, 'seconde')}"
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,49 +8,17 @@ class Comment < ActiveRecord::Base
   def is_root?
     !reply_to || reply_to == 0
   end
-  def text
-    format read_attribute(:text)
-  end
-  def format comment
-    if comment
-      comment = format_carriage_return comment
-      comment = format_links comment
-    end  
-    comment
-  end
-  def format_carriage_return comment
-      comment = comment.gsub(/(\r\n|\n)/, '<br />')
-      comment.gsub(/(<br \/>){3,20}/, '<br />')
-  end
-  def format_links comment
-    regex = /[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}[A-Za-z0-9\-\.\+\_\/\%\?\#\&\=]*/
-    comment = comment.gsub(/http:\/\//, '')
-    comment.gsub(regex, '<a target="blank" href="http://\0">\0</a>')
-  end
+  
   def since
     created_at = read_attribute(:created_at).to_datetime
     hours,minutes,seconds = Date.day_fraction_to_time(DateTime.now - created_at)
-    friendly_display(hours, minutes, seconds)
-  end
-  
-  def friendly_display hours, minutes, seconds
+    
     if hours > 7 * 24
       return created_at.strftime("%Y-%m-%d")
-    elsif (24..(24 * 7)).include?(hours)
-      if (24..47).include?(hours)
-        return "Depuis une journée"
-      else  
-        return "Depuis #{hours / 24} jours"
-      end
-    elsif (2..23).include?(hours)
-      return "Depuis #{hours} heures"
-    elsif hours == 1
-      return "Depuis une heure"
-    elsif (2..59).include?(minutes)
-      return "Depuis #{minutes} minutes"
-    elsif minutes == 1
-      return "Depuis une minute"
-    end 
-    return "Depuis #{seconds} secondes"
+    elsif block_given?
+      return yield hours, minutes, seconds
+    else
+      return "#{hours} #{minutes} #{seconds}"
+    end
   end
 end
