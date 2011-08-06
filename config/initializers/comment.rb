af6c97cc56a47e9a1c2ba3273e145bc799f6fb7e
Jeremy Fabre <jeremy.fabre@hotmail.com>
2011-08-05 23:01:15 -0400

    removal of the comment cleanup for now

--- config/initializers/comment.rb
+++ config/initializers/comment.rb
@@ -1,10 +1,10 @@
 begin 
-  Comment.transaction do
-    comment = Comment.find(461) #|| Comment.find(:all, :conditions => "user LIKE '%Nom%'")
-    comment.delete unless comment.nil?
+  #Comment.transaction do
+    #comment = Comment.find(461) #|| Comment.find(:all, :conditions => "user LIKE '%Nom%'")
+    #comment.delete unless comment.nil?
     #puts comment.inspect
     #comments.each{|c|  }
     #comments.each{|c| c.delete }
-  end
+  #end
 rescue
 end
