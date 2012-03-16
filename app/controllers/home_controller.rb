6db6d41d516a8d10688254c303c075084c2559f2
Jeremy Fabre <jeremy.fabre@hotmail.com>
2012-03-16 08:29:50 -0400

    removed snow and optimised sql in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,11 @@
 class HomeController < ApplicationController
   layout 'master'
   def index
-    @latest_video = Video.all(:order => "created_at DESC").first
-    if(@latest_video.nil?)
-      @latest_video = Video.new(:description => 'No video available')
-    end
-    @latest_pic = Image.in_albums(:order => "created_at DESC").first
+    @latest_video = Video.all(:order => "created_at DESC", :limit => 1).first || Video.new(:description => 'No video available')
+    @latest_pic = Image.in_albums(:order => "created_at DESC", :limit => 1).first
 
     @categories = Category.all
+    
     @posts = Post.all(:conditions => {:is_published => true}, :order => 'published_at DESC').paginate(:page => params[:page], :per_page => 5)
     @title = (@posts.first || Post.new(:title => '')).title
   end
